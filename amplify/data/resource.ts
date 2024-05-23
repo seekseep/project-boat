8ec3b340430c0b6a3bc5045c2a1353d5af903070
YOKOYAMA Taku <seekseep@gmail.com>
2024-05-23 12:29:19 +0900

    add amplify

---
+++ amplify/data/resource.ts
@@ -0,0 +1,29 @@
+import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
+
+const schema = a.schema({
+  Project: a.model({
+    name: a.string()
+  })
+  .authorization(allow => [allow.publicApiKey()]),
+  Opinion: a.model({
+    name: a.string()
+  })
+  .authorization(allow => [allow.publicApiKey()]),
+  Issue: a.model({
+    name: a.string()
+  })
+  .authorization(allow => [allow.publicApiKey()]),
+  Task: a.model({
+    name: a.string()
+  })
+  .authorization(allow => [allow.publicApiKey()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: { expiresInDays: 30 }
+  }
+});
